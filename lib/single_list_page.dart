eb72d48d03691c1c652f4c11c78cfce638908d22
E-d-o <99652381+E-d-o@users.noreply.github.com>
2025-08-11 18:04:45 +0200

    Began working on Single page

--- lib/single_list_page.dart
+++ lib/single_list_page.dart
@@ -16,7 +16,47 @@ class _SingleListPageState extends State<SingleListPage> {
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         actionsPadding: EdgeInsets.symmetric(horizontal: 12),
       ),
-      body: Column(children: [SizedBox(height: 500, width: 500)]),
+      body: BodyContent(),
     );
+  }
+}
+
+class BodyContent extends StatelessWidget {
+  const BodyContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(18),
+      child: Column(children: [_NeededItems(), _AtHomeItems()]),
+    );
+  }
+}
+
+class _NeededItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      padding: EdgeInsets.all(18),
+      width: double.infinity,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Text("Da prendere")],
+          ),
+          Placeholder(fallbackHeight: 100, fallbackWidth: double.infinity),
+        ],
+      ),
+    );
+  }
+}
+
+class _AtHomeItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
   }
 }
